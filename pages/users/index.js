f57d2f39a0ce6d1cd4246367b23e53c7ae205cbe
clever-h <hamid.hrc7@yahoo.com>
2022-12-26 01:02:26 +0330

    Navigating-to-Dynamic-Routes

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,7 +1,24 @@
+import Link from 'next/link';
+
 function UserPage() {
+  const users = [
+    { id: 1, username: 'Saeed' },
+    { id: 2, username: 'Hamid' },
+    { id: 3, username: 'Pasha' },
+    { id: 4, username: 'Reza' },
+    { id: 5, username: 'admin' },
+  ];
+
   return (
     <div>
       <h2>User Page</h2>
+      <ul>
+        {users.map((item) => (
+          <li key={item.id}>
+            <Link href={`/users/${item.id}`}>{item.username}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }

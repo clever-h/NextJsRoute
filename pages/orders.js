6640975dc1bdb4bca30ea95db948fb075e6ff47c
clever-h <hamid.hrc7@yahoo.com>
2023-01-02 00:35:42 +0330

    add ServerJson and Call them

---
+++ pages/orders.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+function OrdersPage() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      const response = await fetch('http://localhost:5000/orders');
+      const responseData = await response.json();
+
+      setOrders(responseData);
+    };
+
+    fetchOrders();
+  }, []);
+
+  return (
+    <div>
+      {orders.map((item) => (
+        <div key={item.id}>
+          {item.username} - {item.amount}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default OrdersPage;
